5414b3a41dccd6f5a662166f61b1a5bfa3ef50cc
AlaFadeli <alaeddinfadeli@gmail:com>
2026-07-28 17:21:16 +0100

    transform, filter and add revenue

---
+++ pipeline/transformer.py
@@ -0,0 +1,22 @@
+# Helper Function
+def to_num(v):
+    try:
+        return float(v)
+    except ValueError:
+        return v
+
+
+# Transform float str to float
+def parse_numeric(rows):
+    return [{k: to_num(v) for k, v in row.items()} for row in rows]
+
+
+# Filter by value of key
+def filter_by(rows, key, value):
+    filtered = [row for row in rows if row[key] == value]
+    return filtered
+
+
+# Add revenue
+def add_revenue(rows):
+    return [{**r, "revenue": r["quantity"] * r["unit_price"]} for r in rows]
